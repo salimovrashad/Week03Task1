bf9f74cb60287d24dd8c5db47c1a86abdc944479
salimovrashad <selimovresad33@gmail.com>
2023-10-16 18:31:21 +0400

    add task files

---
+++ ConsoleApp.Loops/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            //int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3, 1, 2 };
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] == -1)
+            //    {
+            //        continue;
+            //    }
+
+            //    int count = 1;
+
+            //    for (int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if (arr[j] == arr[i])
+            //        {
+            //            count++;
+            //            arr[j] = -1;
+            //        }
+            //    }
+
+            //    if (count > 1)
+            //    {
+            //        Console.WriteLine($"{arr[i]} {count} defe tekrarlanir.");
+            //    }
+            //}
+
+            //Task 2
+            //Console.WriteLine("Arrayin olcusunu yazin: ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int[] arr = new int[size];
+
+            //Console.WriteLine("Element daxil edirsiz ?");
+            //string answer = Console.ReadLine();
+
+            //for (int j = 0; j < arr.Length; j++)
+            //{
+            //    if (answer == "yes")
+            //    {
+            //        Console.WriteLine("Element daxil edin...");
+            //        int num = Convert.ToInt32(Console.ReadLine());
+            //        for (int i = 0; i < 1; i++)
+            //        {
+            //            arr[i] = num;
+            //            continue;
+            //            Console.WriteLine(arr[i]);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
